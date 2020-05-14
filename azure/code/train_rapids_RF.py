cbfc75c58a7824f5c877ea2872c7901464a0e396
zronaghi <zronaghi@gmail.com>
2020-05-13 21:18:21 -0700

    Adding larger dataset for multi-GPU

--- azure/code/train_rapids_RF.py
+++ azure/code/train_rapids_RF.py
@@ -56,21 +56,21 @@ def main():
     print('\n---->>>> cuDF version <<<<----\n', cudf.__version__)
     print('\n---->>>> cuML version <<<<----\n', cuml.__version__)
 
-    compute = "single-GPU" # "multi-GPU" option for using multi-GPU algorithms via Dask
-    azure_ml = RapidsCloudML(cloud_type= 'Azure', data_type="Parquet", compute_type=compute)
+    compute = 'single-GPU' # 'multi-GPU' option for using multi-GPU algorithms via Dask
+    azure_ml = RapidsCloudML(cloud_type= 'Azure', data_type='Parquet', compute_type=compute)
     print(compute)
 
-    dataset, _ , y_label, _ = azure_ml.load_data(filename=os.path.join(data_dir, 'airline_20m.parquet'))
+    if compute == 'single-GPU':
+        dataset, _ , y_label, _ = azure_ml.load_data(filename=os.path.join(data_dir, 'airline_20m.parquet'))
+    else:
+       # use parquet files from 'https://airlinedataset.blob.core.windows.net/airline-10years' for multi-GPU training
+       dataset, _ , y_label, _ = azure_ml.load_data(filename=os.path.join(data_dir, 'part*.parquet'), 
+                                                    col_labels = ['Flight_Number_Reporting_Airline',
+                                                                  'Year', 'Quarter', 'Month', 'DayOfWeek',
+                                                                  'DOT_ID_Reporting_Airline', 'OriginCityMarketID', 'DestCityMarketID',
+                                                                  'DepTime', 'DepDelay', 'DepDel15', 'ArrDel15', 'ArrDelay',
+                                                                  'AirTime', 'Distance'], y_label = 'ArrDel15')
     
-    # use parquet files from 'https://airlinedataset.blob.core.windows.net/airline-10years' for multi-GPU training
-    # dataset, _ , y_label, _ = azure_ml.load_data(filename=os.path.join(data_dir, 'part*.parquet'), 
-    #                                             col_labels = ['Flight_Number_Reporting_Airline',
-    #                                                           'Year', 'Quarter', 'Month', 'DayOfWeek',
-    #                                                           'DOT_ID_Reporting_Airline', 'OriginCityMarketID', 'DestCityMarketID',
-    #                                                           'DepTime', 'DepDelay', 'DepDel15', 'ArrDel15',
-    #                                                           'AirTime', 'Distance'], y_label = 'ArrDel15')
-    # X = dataset[dataset.columns.difference([y_label])]
-
     X = dataset[dataset.columns.difference(['ArrDelay', y_label])]
     y = dataset[y_label]
     del dataset
@@ -95,7 +95,7 @@ def main():
 
     # optional cross-validation w/ model_params['n_train_folds'] > 1
     for i_train_fold in range(5):
-        print( f"\n CV fold { i_train_fold } of { 5 }\n" )
+        print( f'\n CV fold { i_train_fold } of { 5 }\n' )
 
         # split data
         X_train, X_test, y_train, y_test, _ = azure_ml.split_data(X, y, random_state =77)
