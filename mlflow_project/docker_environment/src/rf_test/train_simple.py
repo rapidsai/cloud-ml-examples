7110fc7588ca258c0ddacf41605658d6771483fa
Devin Robison <drobison@nvidia.com>
2020-08-21 14:33:32 -0600

    k8s experimentation

--- mlflow_project/docker_environment/src/rf_test/train_simple.py
+++ mlflow_project/docker_environment/src/rf_test/train_simple.py
@@ -22,7 +22,7 @@ def load_data(fpath):
 
     df = cudf.read_parquet(fpath)
     X = df.drop(["ArrDelayBinary"], axis=1)
-    y = df["ArrDelayBinary"].astype('int32')
+    y = df["ArrDelayBinary"].astype("int32")
 
     return train_test_split(X, y, test_size=0.2)
 
@@ -38,17 +38,19 @@ def train(fpath, max_depth, max_features, n_estimators):
     """
     X_train, X_test, y_train, y_test = load_data(fpath)
 
-    mod = RandomForestClassifier(max_depth=max_depth,
-                                 max_features=max_features,
-                                 n_estimators=n_estimators)
+    mod = RandomForestClassifier(
+        max_depth=max_depth, max_features=max_features, n_estimators=n_estimators
+    )
 
     mod.fit(X_train, y_train)
     preds = mod.predict(X_test)
     acc = accuracy_score(y_test, preds)
 
-    mlparams = {"max_depth": str(max_depth),
-                "max_features": str(max_features),
-                "n_estimators": str(n_estimators)}
+    mlparams = {
+        "max_depth": str(max_depth),
+        "max_features": str(max_features),
+        "n_estimators": str(n_estimators),
+    }
     mlflow.log_params(mlparams)
 
     mlflow.log_metric("accuracy", acc)
@@ -58,22 +60,33 @@ def train(fpath, max_depth, max_features, n_estimators):
     return mod
 
 
-if (__name__ == "__main__"):
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--algo', default='tpe', choices=['tpe'], type=str)
-    parser.add_argument('--conda-env', required=True, type=str)
-    parser.add_argument('--fpath', required=True, type=str)
-    parser.add_argument('--n_estimators', type=int, default=100)
-    parser.add_argument('--max_features', type=float, default=1.0)
-    parser.add_argument('--max_depth', type=int, default=12)
+    parser.add_argument("--algo", default="tpe", choices=["tpe"], type=str)
+    parser.add_argument("--conda-env", required=True, type=str)
+    parser.add_argument("--fpath", required=True, type=str)
+    parser.add_argument("--n_estimators", type=int, default=100)
+    parser.add_argument("--max_features", type=float, default=1.0)
+    parser.add_argument("--max_depth", type=int, default=12)
 
     args = parser.parse_args()
 
     experid = 0
 
-    with mlflow.start_run():
-        mlflow.set_tag("mlflow.runName", "RAPIDS-MLFlow-Demo")
-        train(max_depth=args.max_depth,
-              max_features=args.max_features,
-              n_estimators=args.n_estimators,
-              fpath=args.fpath)
+    artifact_path = "Airline-Demo"
+    artifact_uri = None
+    experiment_name = "RAPIDS-CLI"
+    experiment_id = None
+
+    with mlflow.start_run(run_name="RAPIDS-MLFlow"):
+        model = train(args.fpath, args.max_depth, args.max_features, args.n_estimators)
+
+        mlflow.sklearn.log_model(
+            model,
+            artifact_path=artifact_path,
+            registered_model_name="rapids_mlflow_cli",
+            conda_env="envs/conda.yaml",
+        )
+        artifact_uri = mlflow.get_artifact_uri(artifact_path=artifact_path)
+
+    print("Model uri: %s" % artifact_uri)
