2bc9532b80b2985b4942367ee09b8bc3ddbf788b
Devin Robison <drobison@nvidia.com>
2020-09-01 17:28:41 -0600

    Tracking server fixes/customization

--- mlflow_project/docker_environment/src/k8s/tracking_entrypoint.sh
+++ mlflow_project/docker_environment/src/k8s/tracking_entrypoint.sh
@@ -3,6 +3,4 @@
 set -e
 set -x
 
-#postgresql://mlflow_user:mlflow@mlflow-postgres:5432/mlflow_db
-#s3://mlflow-k8s/artifacts
-mlflow server --backend-store-uri=postgresql://${MLFLOW_USER}:${MLFLOW_PASS}@${MLFLOW_DB_ADDR}:${MLFLOW_DB_PORT}/${MLFLOW_DB_NAME} --default-artifact-root=${MLFLOW_ARTIFACT_PATH} --host 0.0.0.0
+mlflow server --backend-store-uri=postgresql://${MLFLOW_USER}:${MLFLOW_PASS}@${MLFLOW_DB_ADDR}:${MLFLOW_DB_PORT}/${MLFLOW_DB_NAME} --default-artifact-root=${MLFLOW_ARTIFACT_PATH} --host 0.0.0.0 --port 80
